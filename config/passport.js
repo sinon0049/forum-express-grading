28d613a7de26c560500c97451b5ecea6aa90875d
wschiang <sinon049@yahoo.com.tw>
2021-02-04 01:59:06 +0800

    feat: add signin page, singin and logout feature

---
+++ config/passport.js
@@ -0,0 +1,31 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+passport.use(new LocalStrategy(
+    { usernameField: 'email', passwordField: 'password', passReqToCallback: true },
+    (req, username, password, done) => {
+        User.findOne({ where: {email: username}})
+            .then(user => {
+                if(!user) return done(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤!'))
+                if(!bcrypt.compareSync(password, user.password)) return done(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤!'))
+                return done(null, user)
+            })
+    }
+))
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findByPk(id)
+        .then(user => {
+            user = user.toJSON()
+            return done(null, user)
+        })
+})
+
+module.exports = passport
