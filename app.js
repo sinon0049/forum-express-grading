18507dd927ac85d9f290ace593c9383bdf43ab30
wschiang <sinon049@yahoo.com.tw>
2021-02-10 20:37:04 +0800

    feat: add category selection in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const helpers = require('./_helpers')
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars({ defaultLayout: 'main', helpers: require('./config/handlebars-helpers') }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave:false, saveUninitialized: false }))
