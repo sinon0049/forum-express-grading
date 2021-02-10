24e391aea121d5ad25e27e6c42c2b8da517a24af
wschiang <sinon049@yahoo.com.tw>
2021-02-10 22:17:01 +0800

    feat: add admin categories edit, put and delete feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,9 +5,17 @@ const categoryController = {
     getCategories: (req, res) => {
         return Category.findAll({ raw: true, nest: true})
             .then(categories => {
-                return res.render('admin/categories', { categories })
+                if(req.params.id) {
+                    Category.findByPk(req.params.id)
+                        .then(category => {
+                            return res.render('admin/categories', { categories, category: category.toJSON() })
+                        })
+                } else {
+                    return res.render('admin/categories', { categories })
+                }
             })
     },
+
     postCategory: (req, res) => {
         if(!req.body.name) {
             req.flash('error_messages', 'please insert name')
@@ -20,8 +28,31 @@ const categoryController = {
             })
         }
     },
-    putCategory: {},
-    deleteCategory: {},
+
+    putCategory: (req, res) => {
+        if(!req.body.name) {
+            req.flash('error_messages', 'name doesn\'t exist')
+            return res.redirect('back')
+        } else {
+            return Category.findByPk(req.params.id)
+            .then(category => {
+                category.update(req.body)
+                    .then(() => {
+                        res.redirect('/admin/categories')
+                    })
+            })
+        }
+    },
+    
+    deleteCategory: (req, res) => {
+        return Category.findByPk(req.params.id)
+            .then(category => {
+                category.destroy()
+                    .then(() => {
+                        res.redirect('/admin/categories')
+                    })
+            })
+    },
 }
 
 module.exports = categoryController
--- routes/index.js
+++ routes/index.js
@@ -38,4 +38,7 @@ module.exports = (app, passport) => {
   app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 }
