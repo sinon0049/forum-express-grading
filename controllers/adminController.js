22d4293c8fb3fa7ccc5f612e1283dca2c68ffee5
wschiang <sinon049@yahoo.com.tw>
2021-02-15 17:51:54 +0800

    add GET /api/admin/restaurants/:id

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,16 +5,12 @@ const Category = db.Category
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const adminService = require('../services/adminService')
 
 const adminController = {
     getRestaurants: (req, res) => {
-      return Restaurant.findAll({
-        raw: true,
-        nest: true,
-        include: [Category]
-      }).then(restaurants => {
-        //console.log(restaurants)
-        return res.render('admin/restaurants', {restaurants: restaurants })
+      adminService.getRestaurants(req, res, (data) => {
+        return res.render('admin/restaurants', data)
       })
     },
 
---
+++ services/adminService.js
@@ -0,0 +1,22 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const User = db.User
+const Category = db.Category
+const fs = require('fs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
+const adminService = {
+    getRestaurants: (req, res, callback) => {
+      return Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: [Category]
+      }).then(restaurants => {
+        //console.log(restaurants)
+        callback({restaurants: restaurants})
+      })
+    },
+}
+
+module.exports = adminService
